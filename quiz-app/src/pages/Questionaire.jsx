74ef593780fb7b84c845ce7ea89362b7f7f26c90
Erniel Caalim <133772245+EMCSquare12@users.noreply.github.com>
2024-05-26 22:01:01 +0800

    fix: preview button to select varies question base on question index

--- quiz-app/src/pages/Questionaire.jsx
+++ quiz-app/src/pages/Questionaire.jsx
@@ -86,23 +86,21 @@ const Questionaire = ({ callbackQuestions, callbackAnswer, data }) => {
     const result = [...answers.resultAnswer];
     const correctAnswerIndex = [...answers.correctAnswerIndex];
 
-    selectedAnswer.push(answerIndex);
-    result.push(correctAnswerIndex[counter] === selectedAnswer[counter]);
-    if (counter !== questions.length) {
-      if (answers.selectedAnswerIndex.length !== questions.length) {
-        setAnswers((prevAnswers) => ({
-          ...prevAnswers,
-          selectedAnswerIndex: selectedAnswer,
-          resultAnswer: result,
-        }));
-        callbackAnswer((prevAnswers) => ({
-          ...prevAnswers,
-          selectedAnswerIndex: selectedAnswer,
-          resultAnswer: result,
-        }));
-        setAnswerIndex(null);
-      } else return;
+    if (answers.selectedAnswerIndex.length < questions.length) {
+      selectedAnswer.push(answerIndex);
+      result.push(correctAnswerIndex[counter] === selectedAnswer[counter]);
     }
+    setAnswers((prevAnswers) => ({
+      ...prevAnswers,
+      selectedAnswerIndex: selectedAnswer,
+      resultAnswer: result,
+    }));
+    callbackAnswer((prevAnswers) => ({
+      ...prevAnswers,
+      selectedAnswerIndex: selectedAnswer,
+      resultAnswer: result,
+    }));
+    setAnswerIndex(null);
 
     console.log(answers.selectedAnswerIndex);
 
@@ -113,17 +111,21 @@ const Questionaire = ({ callbackQuestions, callbackAnswer, data }) => {
     }
   };
 
-  const handlePreview = () => {
+  const handleDoubleClickPreview = () => {
     if (counter < questions.length && counter > 0) {
       setCounter((prevCounter) => prevCounter - 1);
     }
   };
 
-  const handleDoubleClickPreview = (event) => {
-    event.preventDefault();
-    setIsClickPreview(!isClickOutside);
+  const handleClickPreview = () => {
+    setIsClickPreview(!isClickPreview);
   };
 
+  const handlePreviewList = (index) => {
+    setCounter(index);
+    console.log(index);
+    setIsClickPreview(false);
+  };
   const letterSelection = {
     0: "A",
     1: "B",
@@ -131,6 +133,7 @@ const Questionaire = ({ callbackQuestions, callbackAnswer, data }) => {
     3: "D",
   };
 
+  console.log(counter);
   console.log(answerIndex);
   return (
     <div className="md:w-[60%] h-auto w-full md:p-8 p-4 flex flex-col gap-8 ">
@@ -149,12 +152,20 @@ const Questionaire = ({ callbackQuestions, callbackAnswer, data }) => {
         ))}
       </div>
       <div className="flex flex-col items-end w-full h-auto gap-2 -mt-4 ">
-        <div className="w-full h-4 bg-purple-200">
+        <div className="relative w-full h-4 bg-purple-200">
+          <div
+            style={{
+              width: `${
+                (answers.selectedAnswerIndex.length / questions.length) * 100
+              }%`,
+            }}
+            className="absolute z-10 h-full bg-purple-400"
+          ></div>
           <div
             style={{
               width: `${((counter + 1) / questions.length) * 100}%`,
             }}
-            className="z-10 h-full bg-purple-500"
+            className="absolute z-10 h-full bg-purple-600"
           ></div>
         </div>
         <h1 className="text-xs font-medium text-purple-900 md:text-sm right-4 top-6 w-fit">
@@ -162,12 +173,13 @@ const Questionaire = ({ callbackQuestions, callbackAnswer, data }) => {
         </h1>
       </div>
       <div className="flex flex-row justify-between -mt-4">
-        <div className="relative">
+        <div ref={previewRef} className="relative">
           {isClickPreview && answers.selectedAnswerIndex.length > 0 ? (
-            <ul className="absolute bottom-[100%] flex flex-col-reverse w-40 max-h-[200px] overflow-y-auto">
+            <ul className="absolute bottom-[100%] flex flex-col-reverse w-40 max-h-[200px] overflow-y-auto z-10">
               {answers.selectedAnswerIndex.map((_, index) => (
                 <li
                   key={index}
+                  onClick={() => handlePreviewList(index)}
                   className="flex flex-row items-center justify-between w-auto gap-2 px-4 py-2 text-sm font-semibold text-purple-900 border border-purple-200 shadow bg-purple-50 shadow-purple-200 font-poppins md:text-sm hover:bg-purple-200"
                 >
                   {`Question ${index + 1}`}{" "}
@@ -183,9 +195,8 @@ const Questionaire = ({ callbackQuestions, callbackAnswer, data }) => {
             </ul>
           ) : null}
           <button
-            ref={previewRef}
-            onClick={handleDoubleClickPreview}
-            onDoubleClick={handlePreview}
+            onClick={handleClickPreview}
+            onDoubleClick={handleDoubleClickPreview}
             className="flex flex-row items-center gap-2 px-4 py-2 text-sm font-semibold text-purple-900 border-2 border-purple-500 font-poppins md:text-base hover:shadow-md hover:shadow-purple-200"
           >
             {counter + 1 === 1 ? null : <IoMdArrowDropleftCircle />}
