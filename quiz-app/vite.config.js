3b183ac29538f316b373168d0946256a1a2d1bfe
Erniel Caalim <133772245+EMCSquare12@users.noreply.github.com>
2024-05-30 12:55:40 +0800

    feat: add web icon

--- quiz-app/vite.config.js
+++ quiz-app/vite.config.js
@@ -5,13 +5,11 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0", // Binds to all network interfaces
-    port: 3000, // Change this if you need a different port
+    host: "0.0.0.0", //
+    port: 3000, //
   },
   build: {
     outDir: "dist",
-    rollupOptions: {
-      // Customize the options as needed
-    },
+   
   },
 });
